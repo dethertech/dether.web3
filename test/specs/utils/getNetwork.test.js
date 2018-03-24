aadadf01fac21e7db396e3bcb2267d3ae8f2b716
ishak <ishak@dether.io>
2018-03-24 16:37:54 +0100

    [UPD] tests

--- test/specs/utils/getNetwork.test.js
+++ test/specs/utils/getNetwork.test.js
@@ -3,17 +3,15 @@ import { expect } from 'chai';
 
 import DetherWeb3 from '../../../src';
 
-describe('dether web3 getAddress', () => {
-  describe('instanciation', () => {
-    let detherWeb3;
-    it('should instancite get eth address', async () => {
-      try {
-        detherWeb3 = new DetherWeb3();
-        const network = await detherWeb3.getNetwork();
-        expect(network).to.not.equal(null);
-      } catch (e) {
-        expect(e).to.equal(null);
-      }
-    });
+describe('dether web3 getNetwork', () => {
+  let detherWeb3;
+  it('should get network ID', async () => {
+    try {
+      detherWeb3 = new DetherWeb3();
+      const network = await detherWeb3.getNetwork();
+      expect(network).to.not.equal(null);
+    } catch (e) {
+      expect(e).to.equal(null);
+    }
   });
 });
--- test/specs/wallet/getAddress.test.js
+++ test/specs/wallet/getAddress.test.js
@@ -4,17 +4,15 @@ import { expect } from 'chai';
 import DetherWeb3 from '../../../src';
 
 describe('dether web3 getAddress', () => {
-  describe('instanciation', () => {
-    let detherWeb3;
-    it('should instancite get eth address', async () => {
-      try {
-        detherWeb3 = new DetherWeb3();
-        const addr = await detherWeb3.getAddress();
-        /* eslint-disable-next-line */
-        expect(/^0x[a-fA-F0-9]{40}$/.test(addr)).to.be.true;
-      } catch (e) {
-        expect(e).to.equal(null);
-      }
-    });
+  let detherWeb3;
+  it('should get eth address', async () => {
+    try {
+      detherWeb3 = new DetherWeb3();
+      const addr = await detherWeb3.getAddress();
+      /* eslint-disable-next-line */
+      expect(/^0x[a-fA-F0-9]{40}$/.test(addr)).to.be.true;
+    } catch (e) {
+      expect(e).to.equal(null);
+    }
   });
 });
